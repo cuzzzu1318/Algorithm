6403e18cff397d4b2466be0ec01cab9be45baf17
Jeongho <cuzzzu1318@naver.com>
2022-07-18 16:13:18 +0900

    BaekJun 22-07-18

---
+++ Baekjoon/src/BJ10869.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ10869 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A;
+		int B;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		System.out.println(A+B);
+		System.out.println(A-B);
+		System.out.println(A*B);
+		System.out.println(A/B);
+		System.out.println(A%B);
+	}
+}
---
+++ Baekjoon/src/BJ10926.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ10926 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String ID;
+		ID = sc.nextLine();
+		System.out.println(ID+"??!");
+	}
+}
