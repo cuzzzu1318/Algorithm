4135ef8097ff6f7e1851ef2b271e15ea91ebe807
Jeongho <cuzzzu1318@naver.com>
2023-03-07 19:22:27 +0900

    2023.03.07

--- Baekjoon/src/mar_2023/BOJ7568.java
+++ Baekjoon/src/mar_2023/BOJ7568.java
@@ -1,5 +1,20 @@
 package mar_2023;
 
-public class BOJ7568 {
+/*  
+ * 난이도 : 
+ * 아이디어: 
+ * 시간복잡도:
+ * 소요 시간: 
+ * */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BOJ7568 {
+   public static void main(String[] args) throws NumberFormatException, IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+   }
 }
