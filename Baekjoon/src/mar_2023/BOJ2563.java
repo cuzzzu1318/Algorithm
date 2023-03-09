a23f6a005863c5db2197590e17b2374b71296428
Jeongho <cuzzzu1318@naver.com>
2023-03-09 16:36:17 +0900

    2023.03.08

--- Baekjoon/src/mar_2023/BOJ2563.java
+++ Baekjoon/src/mar_2023/BOJ2563.java
@@ -12,32 +12,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class BOJ7568 {
+public class BOJ2563 {
    public static void main(String[] args) throws NumberFormatException, IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      int N = Integer.parseInt(br.readLine());
+      int n = Integer.parseInt(br.readLine());
       ArrayList<int[]> arr = new ArrayList<>();
-      int[] ans = new int[N];
-      for(int i = 0;i<N;i++) {
+      
+      int[][] ans = new int[100][100];
+      
+      for(int i = 0;i<n;i++) {
     	  int[] temp = new int[2];
     	  String[] s = br.readLine().split(" ");
     	  temp[0] = Integer.parseInt(s[0]);
+    	  
     	  temp[1] = Integer.parseInt(s[1]);
+    	  for(int x = temp[0];x<temp[0]+10;x++) {
+    		  for(int y = temp[1];y<temp[1]+10;y++) {
+        		  ans[x][y]=1;
+        	  }
+    	  }
     	  arr.add(temp);
       }
-      
-      for(int i = 0;i<N;i++) {
-    	  ans[i]=1;
-    	  for(int j = 0;j<N;j++) {
-    		  if(arr.get(i)[0]<arr.get(j)[0]&&arr.get(i)[1]<arr.get(j)[1]) {
-    			 ans[i]++; 
+      int cnt = 0;
+      for(int i = 0;i<100;i++) {
+    	  for(int j = 0;j<100;j++) {
+    		  if(ans[i][j]==1) {
+    			  cnt++;
     		  }
     	  }
       }
-      
-      for(int i : ans) {
-    	  System.out.print(i+" ");
-      }
+      System.out.println(cnt);
       
       
    }
