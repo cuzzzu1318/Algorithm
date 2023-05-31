14786e1460ae5456b95d3455918211d8491275c2
Jeongho <cuzzzu1318@naver.com>
2023-05-31 09:27:08 +0900

    23.05.31

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -2,38 +2,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
-
+	static BigInteger[][] v;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		Queue<Integer> q = new LinkedList<>();
 		
-		for (int i = 1; i <= N; i++) {
-			q.offer(i);
-		}
-		int cnt = 1;
-		sb.append("<");
-		while (q.size()>1) {
-			if (cnt == K) {
-				sb.append(q.poll()+", ");
-				cnt = 1;
-			} else {
-				q.add(q.poll());
-				cnt++;
-			}
-		}
-		sb.append(q.poll()+">");
-		System.out.println(sb);
-
+		
 	}
-
 }
