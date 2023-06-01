576ed5ab90f5f9dbb8490052af4858fb1c4b607f
Jeongho <cuzzzu1318@naver.com>
2023-06-01 14:22:54 +0900

    23.06.01

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -8,13 +8,28 @@ import java.util.StringTokenizer;
 public class Main {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringBuilder sb = new StringBuilder();
-	static BigInteger[][] v;
+	static int N;
+	static int M;
+	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		for(int i= 1;i<=N;i++) {
+			choose(i,1, "");
+		}
+		System.out.println(sb);
 		
-		
+	}
+	
+	static void choose(int n, int m, String str) {
+		str+=Integer.toString(n)+" ";
+		if(m==M) {
+			sb.append(str+"\n");
+			return;
+		}
+		for(int i = n+1;i<=N;i++) {
+			choose(i,m+1,str);
+		}
 	}
 }
