7ff152a32b11954c4b7731ea960d1547ccbf2c6a
Jeongho <cuzzzu1318@naver.com>
2023-06-02 13:58:50 +0900

    23.06.02

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,26 +11,34 @@ public class Main {
 	static StringBuilder sb = new StringBuilder();
 	static int N;
 	static int M;
-	
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		for(int i= 1;i<=N;i++) {
-			choose(i,1, "");
+		int[] nums = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
-		System.out.println(sb);
+		Arrays.sort(nums);
+		for(int i= 0;i<N;i++) {
+			makeArr(nums, i, 1, "");
+		}
 		
+		System.out.println(sb);
+
 	}
-	
-	static void choose(int n, int m, String str) {
-		str+=Integer.toString(n)+" ";
-		if(m==M) {
-			sb.append(str+"\n");
+
+	static void makeArr(int[] num,int n, int m, String str) {
+		str += Integer.toString(num[n]) + " ";
+		if (m == M) {
+			sb.append(str + "\n");
 			return;
 		}
-		for(int i = n+1;i<=N;i++) {
-			choose(i,m+1,str);
+		for (int i = n; i < N; i++) {
+			makeArr(num, i, m + 1, str);
+			
 		}
 	}
 }
