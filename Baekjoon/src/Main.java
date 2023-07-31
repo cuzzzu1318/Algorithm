b20015ba35455851ce5b3316211abacfe89bcf15
Jeongho <cuzzzu1318@naver.com>
2023-07-31 16:56:12 +0900

    23.07.31

--- Baekjoon/src/Main.java
+++ Baekjoon/src/Main.java
@@ -1,64 +1,99 @@
 import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-	static int[] dy = { -1, 0, 1, 0 };
-	static int[] dx = { 0, 1, 0, -1 };
-	static StringBuilder sb = new StringBuilder();
-	static int N;
-	static char[][] arr;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	// 좌측상단에서 우측 하단으로 내려가기 때문에 8방향을 다 보는 것이 아닌 4방향만 탐색
+	// 다른 방향을 탐색하게 되면 좌측 바둑알만을 출력할 수 없음.
+	// 0: ↗, 1: →, 2: ↘, 3: ↓ 총 네 방향
+	static int[] dy = { -1, 0, 1, 1};
+	static int[] dx = { 1, 1, 1, 0};
+
+	static int[][] board = new int[19][19];
+	static boolean[][] v = new boolean[19][19];
+
+	public static void main(String[] args) throws Exception {
+		//////////////////////////////////////////////////////////////
+		// 테스트 후 아래 파일 입력을 표준입력으로 처리하는 문장은 주석 처리해주세요!!!! ( System.setIn처리 코드 )
+		//////////////////////////////////////////////////////////////
+//		System.setIn(new FileInputStream("Test5.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		arr = new char[4 * N - 1][4 * N - 3];
-		for (char[] c : arr) {
-			Arrays.fill(c, ' ');
-		}
-		int y = 2 * N;
-		int x = 2 * (N - 1);
-		arr[y][x] = '*';
-		if (N == 1) {
-			System.out.println("*");
-		} else {
-			draw(y, x);
-			for(int i = 0;i<arr.length;i++) {
-				for(int j= 0;j<arr[0].length;j++) {
-					if(i==1&&j>0) {
-						break;
-					}
-					sb.append(arr[i][j]);
-				}
-				sb.append("\n");
+
+		// 승자의 정보를 [색, y좌표, x좌표}로 저장.
+		int[] winner = new int[3];
+
+		// 입력
+		for (int i = 0; i < 19; i++) {
+			String[] s = br.readLine().split(" ");
+			for (int j = 0; j < 19; j++) {
+				board[i][j] = Integer.parseInt(s[j]);
 			}
-			System.out.println(sb);
 		}
+
+		// 모든 오목판을 탐색
+		loop: for (int i = 0; i < 19; i++) {
+			for (int j = 0; j < 19; j++) {
+				int color = board[j][i];
+				// 해당 칸에 오목알이 있다면
+				if (color > 0) {
+					// 네 방향에 대해 탐색 시작
+					for (int d = 0; d < 4; d++) {
+						// 이긴 것이 확정이라면 승자로 저장 후 반복문 종료
+						if (isWin(color, j, i, d)) {
+							winner[0] = color;
+							// 배열의 인덱스는 0부터 시작이므로 1씩 더한 값 저장
+							winner[1] = j + 1;
+							winner[2] = i + 1;
+							break loop;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(winner[0]);
+		if (winner[0] != 0) {
+			System.out.println(winner[1] + " " + winner[2]);
+		}
+
 	}
 
-	static void draw(int y, int x) {
-		int len = 0;
+	// 승자 판단 함수
+	static boolean isWin(int color, int y, int x, int d) {
+		int cnt = 1;
+		int ny = y;
+		int nx = x;
 		while (true) {
-			for (int d = 0; d < 4; d++) {
-				switch (d) {
-				case 0:
-					len += 2;
-					break;
-				case 2:
-					len += 2;
-					break;
-				}
-				for (int i = 0; i < len; i++) {
-					y += dy[d];
-					x += dx[d];
-					if (x >= arr[0].length) {
-						return;
-					}
-					arr[y][x] = '*';
-				}
+			ny += dy[d];
+			nx += dx[d];
+			if (isValid(ny, nx) && board[ny][nx] == color && !v[ny][nx]) {
+				cnt++;
+			} else {
+				break;
 			}
 		}
+		ny = y;
+		nx = x;
+		while (true) {
+			ny -= dy[d];
+			nx -= dx[d];
+			if (isValid(ny, nx) && board[ny][nx] == color && !v[ny][nx]) {
+				cnt++;
+			} else {
+				break;
+			}
+		}
+		if(cnt==5) {
+			return true;
+		}
+		return false;
 
+	}
+
+	// 범위 유효성 판단 함수
+	static boolean isValid(int y, int x) {
+		if (y < 0 || x < 0 || y >= 19 || x >= 19)
+			return false;
+		return true;
 	}
 }
