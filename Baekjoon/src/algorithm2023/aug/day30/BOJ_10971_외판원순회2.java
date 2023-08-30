224a4fa608aaf1d75b48e7094d03e4629e2ebe4f
Jeongho <cuzzzu1318@naver.com>
2023-08-30 22:16:19 +0900

    23.08.30

--- Baekjoon/src/algorithm2023/aug/day30/BOJ_10971_외판원순회2.java
+++ Baekjoon/src/algorithm2023/aug/day30/BOJ_10971_외판원순회2.java
@@ -39,9 +39,11 @@ public class BOJ_10971_외판원순회2 {
 	static void perm(int idx) {
 		if (idx == N) {
 			int sum = 0;
-			for (int i = 1; i <= N; i++) {
-				sum += graph[selected[i-1]][selected[i%N]];
+			for (int i = 1; i < N; i++) {
+				sum += graph[selected[i-1]][selected[i]];
 			}
+			if(graph[selected[N-1]][selected[0]]==0)return;
+			sum+=graph[selected[N-1]][selected[0]];
 			min = Math.min(sum, min);
 			return;
 		}
