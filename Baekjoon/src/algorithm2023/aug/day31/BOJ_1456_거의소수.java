f90e85bf5d95f03d7f20962e47e659b4c23cbbf4
cuzzzu1318 <cuzzzu1318@naver.com>
2023-08-31 17:44:22 +0900

    23.08.31

---
+++ Baekjoon/src/algorithm2023/aug/day31/BOJ_1456_거의소수.java
@@ -0,0 +1,14 @@
+package algorithm2023.aug.day31;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_1456_거의소수 {
+	static int N;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		
+	}
+}
