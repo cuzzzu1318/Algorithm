d66d664953170b119e5f6f215e9b1098e4a5c86b
cuzzzu1318 <cuzzzu1318@naver.com>
2023-09-07 08:55:56 +0900

    23.09.07

--- Baekjoon/src/algorithm2023/sep/day05/BOJ_17404_RGB거리2.java
+++ Baekjoon/src/algorithm2023/sep/day05/BOJ_17404_RGB거리2.java
@@ -5,13 +5,13 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_17404_RGB거리2 {
-	static int N, cost[][], zonZalZungHo[][], ans;
+	static int N, cost[][], dp[][], ans;
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(br.readLine());
 		cost = new int[N][3];
-		zonZalZungHo = new int[N][3];
+		dp = new int[N][3];
 		ans = Integer.MAX_VALUE;
 		for(int i =0;i<N;i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
@@ -21,40 +21,40 @@ public class BOJ_17404_RGB거리2 {
 		}
 		
 		
-		zonZalZungHo[0][0] = 100000;
-		zonZalZungHo[0][1] = cost[0][1];
-		zonZalZungHo[0][2] = cost[0][2];
+		dp[0][0] = 100000;
+		dp[0][1] = cost[0][1];
+		dp[0][2] = cost[0][2];
 		
 		for(int i = 1;i<N;i++) {
-			zonZalZungHo[i][0] = cost[i][0]+Math.min(zonZalZungHo[i-1][1], zonZalZungHo[i-1][2]);
-			zonZalZungHo[i][1] = cost[i][1]+Math.min(zonZalZungHo[i-1][0], zonZalZungHo[i-1][2]);
-			zonZalZungHo[i][2] = cost[i][2]+Math.min(zonZalZungHo[i-1][1], zonZalZungHo[i-1][0]);
+			dp[i][0] = cost[i][0]+Math.min(dp[i-1][1], dp[i-1][2]);
+			dp[i][1] = cost[i][1]+Math.min(dp[i-1][0], dp[i-1][2]);
+			dp[i][2] = cost[i][2]+Math.min(dp[i-1][1], dp[i-1][0]);
 		}
-		ans = Math.min(ans, zonZalZungHo[N-1][0]);
+		ans = Math.min(ans, dp[N-1][0]);
 		
-		zonZalZungHo[0][0] = cost[0][0];
-		zonZalZungHo[0][1] = 1000000;
-		zonZalZungHo[0][2] = cost[0][2];
+		dp[0][0] = cost[0][0];
+		dp[0][1] = 1000000;
+		dp[0][2] = cost[0][2];
 		
 		for(int i = 1;i<N;i++) {
-			zonZalZungHo[i][0] = cost[i][0]+Math.min(zonZalZungHo[i-1][1], zonZalZungHo[i-1][2]);
-			zonZalZungHo[i][1] = cost[i][1]+Math.min(zonZalZungHo[i-1][0], zonZalZungHo[i-1][2]);
-			zonZalZungHo[i][2] = cost[i][2]+Math.min(zonZalZungHo[i-1][1], zonZalZungHo[i-1][0]);
+			dp[i][0] = cost[i][0]+Math.min(dp[i-1][1], dp[i-1][2]);
+			dp[i][1] = cost[i][1]+Math.min(dp[i-1][0], dp[i-1][2]);
+			dp[i][2] = cost[i][2]+Math.min(dp[i-1][1], dp[i-1][0]);
 		}
 		
-		ans = Math.min(ans, zonZalZungHo[N-1][1]);
+		ans = Math.min(ans, dp[N-1][1]);
 		
-		zonZalZungHo[0][0] = cost[0][0];
-		zonZalZungHo[0][1] = cost[0][1];
-		zonZalZungHo[0][2] = 1000000;
+		dp[0][0] = cost[0][0];
+		dp[0][1] = cost[0][1];
+		dp[0][2] = 1000000;
 		
 		for(int i = 1;i<N;i++) {
-			zonZalZungHo[i][0] = cost[i][0]+Math.min(zonZalZungHo[i-1][1], zonZalZungHo[i-1][2]);
-			zonZalZungHo[i][1] = cost[i][1]+Math.min(zonZalZungHo[i-1][0], zonZalZungHo[i-1][2]);
-			zonZalZungHo[i][2] = cost[i][2]+Math.min(zonZalZungHo[i-1][1], zonZalZungHo[i-1][0]);
+			dp[i][0] = cost[i][0]+Math.min(dp[i-1][1], dp[i-1][2]);
+			dp[i][1] = cost[i][1]+Math.min(dp[i-1][0], dp[i-1][2]);
+			dp[i][2] = cost[i][2]+Math.min(dp[i-1][1], dp[i-1][0]);
 		}
 		
-		ans = Math.min(ans, zonZalZungHo[N-1][2]);
+		ans = Math.min(ans, dp[N-1][2]);
 		
 		System.out.println(ans);
 	}
---
+++ Baekjoon/src/algorithm2023/sep/day06/BOJ_2133_타일채우기.java
@@ -0,0 +1,17 @@
+package algorithm2023.sep.day06;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2133_타일채우기 {
+	static int N, map[][];
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[3][N+1];
+		
+		
+		
+	}
+}
