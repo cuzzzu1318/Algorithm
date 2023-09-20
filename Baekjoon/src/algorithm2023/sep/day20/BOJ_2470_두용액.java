36c5c5f0dc62d1cd913977889d39959183cc9d96
cuzzzu1318 <cuzzzu1318@naver.com>
2023-09-21 01:19:22 +0900

    23.09.20

--- Baekjoon/src/algorithm2023/sep/day20/BOJ_2470_두용액.java
+++ Baekjoon/src/algorithm2023/sep/day20/BOJ_2470_두용액.java
@@ -11,26 +11,59 @@ public class BOJ_2470_두용액 {
 	static StringBuilder sb = new StringBuilder();
 
 	static int N, liquid[], start, end;
-	
+	static int min = Integer.MAX_VALUE;
+
 	public static void main(String[] args) throws Exception {
 		N = Integer.parseInt(br.readLine());
 		liquid = new int[N];
 		start = 0;
-		end = N-1;
-		
+		end = N - 1;
+
 		st = new StringTokenizer(br.readLine());
-		for(int i =0 ;i<N;i++) {
+		for (int i = 0; i < N; i++) {
 			liquid[i] = Integer.parseInt(st.nextToken());
 		}
-		
+
 		Arrays.sort(liquid);
-		System.out.println(Arrays.toString(liquid));
-		binarySearch(start,end);
-		System.out.println(liquid[start]+" "+liquid[end]);
-		
+		for (int i = 0; i < N; i++) {
+			binarySearch(liquid[i]);
+			if (min == 0)
+				break;
+		}
+
+		System.out.println(start + " " + end);
 	}
-	
-	static void binarySearch(int low, int high) {
+
+	static void binarySearch(int now) {
+		int low = 0;
+		int high = N - 1;
+		int tgt = -now;
+		int middle = -1;
+
+		while (low < high) {
+			middle = (low + high) / 2;
+			if (liquid[middle]!=now&&Math.abs(now + liquid[middle]) < min) {
+				min = Math.abs(now + liquid[middle]);
+				if (now > liquid[middle]) {
+					start = liquid[middle];
+					end = now;
+				} else {
+					start = now;
+					end = liquid[middle];
+				}
+			}
+			if (liquid[middle] == tgt) {
+				break;
+			}
+			if (liquid[middle] < tgt) {
+				low = middle + 1;
+			}
+			if (liquid[middle] > tgt) {
+				high = middle;
+			}
+			
+		}
 		
+
 	}
 }
