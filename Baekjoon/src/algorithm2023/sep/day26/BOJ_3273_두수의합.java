ffaf4fbefc708d68f48cbd5b1d616c38d779001b
cuzzzu1318 <cuzzzu1318@naver.com>
2023-09-26 10:26:13 +0900

    23.09.26

---
+++ Baekjoon/src/algorithm2023/sep/day26/BOJ_3273_두수의합.java
@@ -0,0 +1,32 @@
+package algorithm2023.sep.day26;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_3273_두수의합 {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb = new StringBuilder();
+	
+	static int n, arr[], x;
+	
+	
+	public static void main(String[] args) throws Exception{
+		n = Integer.parseInt(br.readLine());
+		arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i =0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		x = Integer.parseInt(br.readLine());
+		
+		
+		
+		
+		
+		
+		
+	}
+}	
