656e9e66af3df0e215ae6635141330801be5a121
정호 <cuzzzu1318@naver.com>
2024-09-24 16:44:21 +0900

    24.09.22

--- Baekjoon/src/algorithm2024/sep/day22/BOJ_22251_빌런호석_bit.java
+++ Baekjoon/src/algorithm2024/sep/day22/BOJ_22251_빌런호석_bit.java
@@ -1,28 +1,18 @@
 package algorithm2024.sep.day22;
 
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
-public class BOJ_22251_빌런호석 {
+public class BOJ_22251_빌런호석_bit {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
 
-    static int N, K, P;
-    static String X;
+    static int N, K, P, X;
 
-    static boolean[][] nums = {
-            {true, true, true, false, true, true, true},
-            {false, false, true, false, false, true, false},
-            {true, false, true, true, true, false, true},
-            {true, false, true, true, false, true, true},
-            {false, true, true, true, false, true, false},
-            {true, true, false, true, false, true, true},
-            {true, true, false, true, true, true, true},
-            {true, false, true, false, false, true, false},
-            {true, true, true, true, true, true, true, true},
-            {true, true, true, true, false, true, true}
-    };
+    static int[] nums = {119, 18, 93, 91, 58, 107, 111, 82, 127, 123};
 
 
     public static void main(String[] args) throws Exception {
@@ -30,31 +20,25 @@ public class BOJ_22251_빌런호석 {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
         P = Integer.parseInt(st.nextToken());
-        X = st.nextToken();
+        X = Integer.parseInt(st.nextToken());
 
-//        X의 앞자리에 0을 채워주는 작업
-        while(X.length()<K){
-            X = new StringBuilder("0").append(X).toString();
-        }
 
 //        X를 숫자의 배열로 변경
         int[] floor = new int[K];
-        for (int i = 0; i < X.length(); i++) {
-            floor[i] = X.charAt(i) - '0';
+        for (int i = 0; i < K; i++) {
+            floor[i] = X%(int)Math.pow(10, K-i)/(int)Math.pow(10, K-1-i);
         }
 
         int ans =0 ;
         for (int i = 1; i <= N; i++) {
 //            각 자리별로 숫자들과 비교해보고 바꿔야 하는 LED 수가 P이하라면 카운트.
-            if (i == Integer.parseInt(X)) continue;
+            if (i == X) continue;
             int cnt = 0;
             int idx = 0;
 //            첫번째 자릿수부터 변경해야 하는 횟수 카운트
             for (int j = (int) Math.pow(10, K-1); j > 0; j/=10) {
                 int n = i%(j*10)/j;
-                for (int k = 0; k < 7; k++) {
-                    if(nums[n][k]!=nums[floor[idx]][k])cnt++;
-                }
+                cnt+=Integer.bitCount(nums[n]^nums[floor[idx]]);
                 idx++;
             }
             if(cnt<=P) ans++;
